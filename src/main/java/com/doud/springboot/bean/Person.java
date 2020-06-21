b43dfd116026760c28977cac529238c6d2a41a8a
lingtao <lingtao@localhost.localdomain>
2020-06-21 21:36:49 +0800

    Initial commit

---
+++ src/main/java/com/doud/springboot/bean/Person.java
@@ -0,0 +1,41 @@
+package com.doud.springboot.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 将配置文件中的每一个属性，映射到这个组件中
+ * @ConfigurationProperties： 告诉springBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ *     prefix= "person" ，配置文件中那个下面的所有属性进行一一映射
+ * 只有这个组件是容器中的组件，才能为容器提供的@ConfigurationProperties功能
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
