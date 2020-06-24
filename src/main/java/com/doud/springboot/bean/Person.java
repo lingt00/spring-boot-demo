ddf5c1e59cfba7543e645383bc099f5019aa1a93
lingt00 <zi_se_chen_ai@163.com>
2020-06-24 14:58:54 +0800

    init

--- src/main/java/com/doud/springboot/bean/Person.java
+++ src/main/java/com/doud/springboot/bean/Person.java
@@ -3,6 +3,8 @@ package com.doud.springboot.bean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 /**
  * 将配置文件中的每一个属性，映射到这个组件中
  * @ConfigurationProperties： 告诉springBoot将本类中的所有属性和配置文件中相关的配置进行绑定
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class Person {
     private String name;
     private int age;
+    private Map<String,String> info;
 
     public String getName() {
         return name;
@@ -31,11 +34,20 @@ public class Person {
         this.age = age;
     }
 
+    public Map<String, String> getInfo() {
+        return info;
+    }
+
+    public void setInfo(Map<String, String> info) {
+        this.info = info;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", info=" + info +
                 '}';
     }
 }
