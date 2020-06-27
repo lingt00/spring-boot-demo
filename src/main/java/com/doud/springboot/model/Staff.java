0dacd476fcdb9ec586d7c1e891714cd8dd3270a5
lingt00 <zi_se_chen_ai@163.com>
2020-06-27 23:05:26 +0800

    mybatis、junit测试

--- src/main/java/com/doud/springboot/model/Staff.java
+++ src/main/java/com/doud/springboot/model/Staff.java
@@ -1,8 +1,10 @@
 package com.doud.springboot.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Staff {
+public class Staff implements Serializable {
+    private static final long serialVersionUID = 3239721973017506182L;
     /**
      * 主键
      */
--- src/test/java/com/doud/springboot/SpringBootConfigApplicationTest.java
+++ src/test/java/com/doud/springboot/SpringBootConfigApplicationTest.java
@@ -1,10 +1,16 @@
 package com.doud.springboot;
 
 import com.doud.springboot.bean.Person;
+import com.doud.springboot.dao.StaffMapper;
+import com.doud.springboot.model.Staff;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -21,6 +27,13 @@ public class SpringBootConfigApplicationTest {
     @Autowired
     DataSource dataSource;
 
+    @Autowired
+    StaffMapper staffMapper;
+
+    TestRestTemplate template = new TestRestTemplate();
+
+    String root = "http://127.0.0.1:8080";
+
     @Test
     public void testConfig(){
         System.out.println(person);
@@ -33,4 +46,17 @@ public class SpringBootConfigApplicationTest {
         System.out.println(connection);
         connection.close();
     }
+
+    @Test
+    public void testStaff(){
+        System.out.println(staffMapper.countByExample(null));
+    }
+
+    @Test
+    public void testStaffList(){
+       String result = template.getForObject(root+"/staff",String.class);
+
+       Assert.assertNotNull("访问失败",result);
+    }
+
 }
